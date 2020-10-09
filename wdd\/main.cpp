2d6e158a39d3b5cb17fef204fda095bb408f3048
srgbrs <borisov11092000@gmail.com>
2020-10-09 22:48:25 +0300

    Initial Commit

---
+++ wdd\/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wdd\
+//
+//  Created by Sergey on 10/9/20.
+//  Copyright © 2020 sergey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
